f8e8f22f511a7443c2384e5d208fb21c057a946e
unknown <evallardy@gmail.com>
2025-09-04 21:28:24 -0600

    Nueva version 5

--- core/views.py
+++ core/views.py
@@ -314,14 +314,15 @@ def home(request):
     return render(request, 'core/cliente/home.html', { 'context': context })
 class BaseClienteView(TemplateView):
     mensaje_cliente = "Mensajes para el cliente"
-    producto = Inventario.objects.all()[:1].get()
-    producto_mas_vendido = producto.descripcion
-    producto_mas_vendido_id = producto.id
-    producto_precios = Inventario.objects.filter(id=producto_mas_vendido_id) \
+#    producto = Inventario.objects.all()[:1].get()
+#    producto_mas_vendido = producto.descripcion
+#    producto_mas_vendido_id = producto.id
+    producto_precios = Inventario.objects.filter(id=1) \
     .aggregate(
         precio_minimo=Min('precio'),
         precio_maximo=Max('precio')
     )
+    
     precio_maximo = producto_precios['precio_maximo'] or 0
     precio_minimo = producto_precios['precio_minimo'] or 0
     alto = 0
